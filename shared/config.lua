9aa1f2a81aacc41a8edb2be78a9e9d5174fb4758
GG79 <midlifegamingcrisis@gmail.com>
2024-01-26 08:47:49 -0800

    added hunting zones

--- shared/config.lua
+++ shared/config.lua
@@ -38,6 +38,8 @@ Config.EnableHuntingBlip = true
 Config.Locations = {
     ["HuntingZones"] = {
         [1] = {label = "Hunting Zone", radius = 650.0, coords = vector3(-938.61, 4823.99, 313.92)},
+        [2] = {label = "Hunting Zone", radius = 650.0, coords = vector3(-772.84, 5461.14, 560.19)},
+        [3] = {label = "Hunting Zone", radius = 650.0, coords = vector3(-1186.72, 3853.52, 489.94)},
     },
     ["NoDispatchZones"] = {
         [1] = {label = "Ammunation 1", coords = vector3(13.53, -1097.92, 29.8), length = 14.0, width = 5.0, heading = 70, minZ = 28.8, maxZ = 32.8},
